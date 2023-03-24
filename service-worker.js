9a10ffb392ffe34d73b907d22420346240d7375a
lukman <bakhtiar.lukman@gmail.com>
2023-03-24 20:39:06 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
-self.addEventListener('fetch', function (event) {
-	event.respondWith(fetch(event.request))
-})
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.open('cocos-cache').then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+});
